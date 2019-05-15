e633c1c8e05fc600eea2498d5548c40a58fc95c3
David Mullins <Mullins6323@gmail.com>
2019-05-14 20:58:10 -0500

    back button working

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,9 +1,27 @@
 import pinsData from '../../helpers/data/pinsData';
+import util from '../../helpers/util';
+
+const bindEvents = () => {
+  document.getElementById('toBoardsBtn').addEventListener('click', () => {
+    document.getElementById('boards-page').classList.remove('hide');
+    document.getElementById('pins-page').classList.add('hide');
+  });
+};
+
+const writePins = (pins) => {
+  let domString = '';
+  pins.forEach((pin) => {
+    domString += `<img src="${pin.imageUrl}" alt="pin image">`;
+  });
+  util.printToDom('pins-on-board', domString);
+};
 
 const initPins = (boardId) => {
+  bindEvents();
   pinsData.loadPinsForBoard(boardId) // this '.then().catch()' resolves the 'new Promise'
     .then((pins) => {
       console.error('allpins', pins);
+      writePins(pins);
     })
     .catch(err => console.error(err));
 };
