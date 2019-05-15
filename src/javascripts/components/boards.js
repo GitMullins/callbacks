17a62951155de9fbbe1415304cdf91555e4175d5
David Mullins <Mullins6323@gmail.com>
2019-05-14 19:24:44 -0500

    boards are printing

--- src/javascripts/components/boards.js
+++ src/javascripts/components/boards.js
@@ -1,9 +1,21 @@
 import boardData from '../helpers/data/boardsData';
+import util from '../helpers/util';
+
+const domStringBuilder = (boards) => {
+  let domString = '';
+  boards.forEach((board) => {
+    domString += '<div class="card col-3">';
+    domString += `<div>${board.id}</div>`;
+    domString += `<div>${board.name}</div>`;
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
   boardData.loadBoards()
     .then((resp) => {
-      console.error('resp', resp.data.boards);
+      domStringBuilder(resp.data.boards);
     })
     .catch(err => console.error('error from loadBoards', err));
 };
